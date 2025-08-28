fbc0397b017226cc9a3b92f309f5ead72ba44501
ansharahamin <adeenfatimahhh@gmail.com>
2025-08-28 18:02:14 +0500

    nested loop

--- app.js
+++ app.js
@@ -56,11 +56,56 @@
 // }
 //     console.log("largest number is " + largest);
 
-var A = [24, 53, 78, 91, 12];
-var smallest = A[0];
-for (i = 0 ; i<A.length; i++){
-    if (A[i] < smallest){
-        smallest =A[i];
+// var A = [24, 53, 78, 91, 12];
+// var smallest = A[0];
+// for (i = 0 ; i<A.length; i++){
+//     if (A[i] < smallest){
+//         smallest =A[i];
+//     }
+// }
+// console.log(smallest + "is smallest no");
+
+// for (var i = 0 ; i<5 ; i++){
+//     console.log(i);
+//     for (var j = 0 ; j<5 ; j++){
+//        console.log(i,j);
+//     }
+    
+// }
+
+// 2d array
+
+// var arr = [
+//     ["ali","dua" , "sara"],
+//     ["john","doe" , "smith"],
+//     ["apple","banana" , "mango"],
+//     [ "a", "b", "c", "d" ],
+   
+// ]
+
+// for (var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+    
+//     for ( var j=0 ; j<arr[i].length;j++){
+//         console.log(arr[i][j]);
+        
+//     }
+// }
+// 3d array
+var numbers = [
+  [[5], [2], [9], [1]],
+  [[8], [7], [3], [4]],
+  [[6], [12], [11], [0]]
+];
+
+for (var i=0;i<numbers.length;i++){
+    row="Row " + (i+1) + ":";
+    
+    for (var j=0;j<numbers[i].length;j++){
+         for (var k=0;k<numbers[i][j].length;k++){
+        // console.log(numbers[i][j][k]);
+        row += numbers[i][j][k] + " ";   // add each number
     }
+    }
+    console.log(row.trim()); // print the entire row at once
 }
-console.log(smallest + "is smallest no");
